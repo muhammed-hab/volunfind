23bc6b03bb3833dbb63b57c2009078c1e982b267
programmer <72678073+muhammed-hab@users.noreply.github.com>
2022-12-13 21:42:59 -0600

    Handling for no results

--- src/results/nonProfitsDisplay.tsx
+++ src/results/nonProfitsDisplay.tsx
@@ -4,12 +4,25 @@ import {useEffect, useState} from "react";
 import {NonProfit} from "./NonProfit";
 import './nonProfitsDisplay.css';
 import {NonProfitResult} from "./NonProfitResult";
+import {Category} from "../Category";
 
 export function NonProfitsDisplay(props: {availability: Availability, categories: string[]}) {
 	const [nonProfits, setNonProfits] = useState<NonProfit[] | undefined>();
+	const [optionsExpanded, setOptionsExpanded] = useState(false);
 	
 	useEffect(() => {
-		NonProfit.getNonProfits(props.availability, props.categories).then(nonprofits => {
+		NonProfit.getNonProfits(props.availability, props.categories).then(async (nonprofits) => {
+			if (nonprofits.length === 0) {
+				setOptionsExpanded(true);
+				
+				const expandedAvailability = Object.fromEntries(
+					Object.entries(props.availability)
+					      .map(([day, avail]) =>
+						           [day, avail.checked ? {checked: true, from: 0, to: 23.5} : avail])
+				) as Availability;
+				const expandedCategories = await Category.getAllCategories();
+				nonprofits = await NonProfit.getNonProfits(expandedAvailability, expandedCategories);
+			}
 			// Not using sort method to avoid also messing with db sorting
 			setNonProfits(nonprofits.filter(({imageURL}) => imageURL)
 			                        .concat(nonprofits.filter(({imageURL}) => !imageURL)));
@@ -38,6 +51,11 @@ export function NonProfitsDisplay(props: {availability: Availability, categories
 						<h2 className="results-title">Results</h2>
 						<a href="#" className="btn btn-blue" onClick={editSearch}>Edit Search</a>
 					</div>
+					{optionsExpanded && (<p className="error-msg" style={{fontSize: 14}}>
+						There were no matches for your availability and interests,
+						so the search criteria was expanded to all categories at any time on your
+						selected days. You can also edit your search using the button above.
+					</p>)}
 					<div className="results-list">
 						{nonProfits.map((nonprofit, idx) =>
 							                <NonProfitResult nonprofit={nonprofit} key={idx} />
